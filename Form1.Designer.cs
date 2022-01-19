0d3f37219f3fc3bbedf7ebb5992de0b7c68933a5
Nayunik <koryukin.danil@list.ru>
2022-01-20 00:42:59 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ShowDevices
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.обновитьСписокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem,
+            this.обновитьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1034, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F10;
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(87, 23);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            this.обновитьToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.обновитьToolStripMenuItem.Size = new System.Drawing.Size(107, 23);
+            this.обновитьToolStripMenuItem.Text = "Обновить список";
+            this.обновитьToolStripMenuItem.Click += new System.EventHandler(this.обновитьToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 59);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1033, 252);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Метка тома";
+            this.columnHeader1.Width = 103;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Название";
+            this.columnHeader2.Width = 134;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Тип файловой системы";
+            this.columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Тип носителя";
+            this.columnHeader4.Width = 97;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Свободное место для данного пользователя (Б)";
+            this.columnHeader5.Width = 257;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Общее свободное место (Б)";
+            this.columnHeader6.Width = 152;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Общий размер (Б)";
+            this.columnHeader7.Width = 107;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обновитьСписокToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 26);
+            // 
+            // обновитьСписокToolStripMenuItem
+            // 
+            this.обновитьСписокToolStripMenuItem.Name = "обновитьСписокToolStripMenuItem";
+            this.обновитьСписокToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.обновитьСписокToolStripMenuItem.Text = "Обновить список";
+            this.обновитьСписокToolStripMenuItem.Click += new System.EventHandler(this.обновитьСписокToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 28);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(482, 27);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Выбор конкретного носителя:";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 311);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 5, 2, 5);
+            this.MinimumSize = new System.Drawing.Size(1050, 350);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Show Devices";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem обновитьСписокToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Diagnostics;
+
+namespace ShowDevices
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            check_device();
+
+            // Получение информации о носителях информации
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady /*&& drive.DriveType == DriveType.Removable*/)
+                {
+                    string[] mas = { drive.Name, drive.VolumeLabel, drive.DriveFormat, drive.DriveType.ToString(), drive.AvailableFreeSpace.ToString(), drive.TotalFreeSpace.ToString(), drive.TotalSize.ToString() };
+                    ListViewItem lvi = new ListViewItem(mas);
+                    listView1.Items.Add(lvi);
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+            await Task.Run(()=> 
+            {
+                while (Process.GetProcessesByName("ShowDevices").Length > 0)
+                {
+                    check_device();
+                }
+            });                        
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор программы:\r\nКорюкин Данил,\r\nгруппа: ИТ-30919.", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void обновитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            comboBox1.Items.Clear();
+            comboBox1.Text = "Выбор конкретного носителя:";
+
+            check_device();
+
+            // Получение информации о носителях информации
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady /*&& drive.DriveType == DriveType.Removable*/)
+                {
+                    string[] mas = { drive.Name, drive.VolumeLabel, drive.DriveFormat, drive.DriveType.ToString(), drive.AvailableFreeSpace.ToString(), drive.TotalFreeSpace.ToString(), drive.TotalSize.ToString() };
+                    ListViewItem lvi = new ListViewItem(mas);
+                    listView1.Items.Add(lvi);
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+        }
+
+        private void обновитьСписокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Выбор конкретного носителя:";
+            listView1.Items.Clear();
+            comboBox1.Items.Clear();
+
+            check_device();
+
+            // Получение информации о носителях информации
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady /*&& drive.DriveType == DriveType.Removable*/)
+                {
+                    string[] mas = { drive.Name, drive.VolumeLabel, drive.DriveFormat, drive.DriveType.ToString(), drive.AvailableFreeSpace.ToString(), drive.TotalFreeSpace.ToString(), drive.TotalSize.ToString() };
+                    ListViewItem lvi = new ListViewItem(mas);
+                    listView1.Items.Add(lvi);
+                    comboBox1.Items.Add(drive.Name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Данный метод проверяет наличие USB устройства к которому "привязана" программа
+        /// </summary>
+        private void check_device()
+        {
+    
+            string request = "SELECT * FROM Win32_USBHub";
+            string checkString = "";
+
+            ManagementObjectSearcher searcer = new ManagementObjectSearcher(request);
+
+            // Формирование строки с перечислением подключенных USB устройств
+            foreach (ManagementBaseObject obj in searcer.Get())
+            {
+                checkString += obj + "\r\n";
+            }
+
+            //Проверка на наличие носителя 
+            if (!checkString.Contains(@"USB\\VID_13FE&PID_4300\\07000367672A3885"))
+            {
+                MessageBox.Show("Отсутствует носитель!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+        }
+           
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string name = comboBox1.Text;
+
+            check_device();
+
+            listView1.Items.Clear();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady /*&& drive.DriveType == DriveType.Removable*/ && drive.Name == name)
+                {
+                    string[] mas = { drive.Name, drive.VolumeLabel, drive.DriveFormat, drive.DriveType.ToString(), drive.AvailableFreeSpace.ToString(), drive.TotalFreeSpace.ToString(), drive.TotalSize.ToString() };
+                    ListViewItem lvi = new ListViewItem(mas);
+                    listView1.Items.Add(lvi);
+                }
+                
+            }
+        }
+
+    }
+}
+
+
